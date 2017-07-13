9b7d4643ca07229a600bea2e68e707950ea689af
Aamir Mughal <waqar.hassan@devbox.co>
2017-07-14 01:09:04 +0500

    airbrake commented

--- config/initializers/erribit.rb
+++ config/initializers/erribit.rb
@@ -1,6 +1,6 @@
-Airbrake.configure do |config|
-  config.api_key = '4c839e51952ccaccb50418c4206d9772'
-  config.host = 'errbit.software‐hut.org.uk'
-  config.port = 443
-  config.secure =config.port == 443
-end
+# Airbrake.configure do |config|
+#   config.api_key = '4c839e51952ccaccb50418c4206d9772'
+#   config.host = 'errbit.software‐hut.org.uk'
+#   config.port = 443
+#   config.secure =config.port == 443
+# end
